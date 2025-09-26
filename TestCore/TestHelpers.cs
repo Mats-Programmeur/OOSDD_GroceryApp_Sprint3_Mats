2a0a193ceecb62214608375651534d42aa2b5869
Mats Bont, de <s1215102@student.windesheim.nl>
2025-09-26 15:04:13 +0200

    Trying to fix Unittests

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -9,37 +9,27 @@ namespace TestCore
         {
         }
 
-
-        //Happy flow
-        [Test]
-        public void TestPasswordHelperReturnsTrue()
-        {
-            string password = "user3";
-            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
-            Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
-        }
-
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08=")]
+        // Happy flow
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=")]
-        public void TestPasswordHelperReturnsTrue(string password, string passwordHash)
+        public void TestPasswordHelperReturnsTrue_ValidHash(string password, string passwordHash)
         {
             Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-
-        //Unhappy flow
-        [Test]
-        public void TestPasswordHelperReturnsFalse()
+        // Unhappy flow
+        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
+        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
+        public void TestPasswordHelperReturnsFalse_InvalidHash(string password, string passwordHash)
         {
-            string password = "user2";
-            string passwordHash = "invalidHash";
             Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
-        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
-        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
+        // Unhappy flow with invalid hash
+        [Test]
+        public void TestPasswordHelperReturnsFalse_WithInvalidHash()
         {
+            string password = "user2";
+            string passwordHash = "invalidHash";
             Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
     }
